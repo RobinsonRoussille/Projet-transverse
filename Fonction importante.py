e8ad8668a6c13213fd4225189274570c74c703e3
Robinson <rsteamsteamr@gmail.com>
2026-02-03 13:57:17 +0100

    Ajout du fichier + fonctions à finir

---
+++ Fonction importante.py
@@ -0,0 +1,23 @@
+def sauter():
+    pass
+
+def se_baisser():
+    pass
+
+def avancer():
+    pass
+
+def reculer():
+    pass
+
+def attraper():
+    pass
+
+def orienter_exctincteur():#Dans le scenario où il y a des flammes uniquement
+    pass
+
+def collision():
+    pass
+
+if __name__=="__main__":
+    pass
